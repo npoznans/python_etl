5ff8fb3105c715dded77fcb373762a5bf0afbfa4
Nate Poznanski-Ring <nsring@me.com>
2017-11-01 11:30:32 -0700

    cleaning up stuff

--- etl_base.py
+++ etl_base.py
@@ -5,15 +5,15 @@ import psycopg2
 triggers = []
 
 try:
-    conn = psycopg2.connect("dbname='well_info_development' host='localhost' user='postgres' password='postgres'")
+    conn = psycopg2.connect("dbname='local_dev' host='localhost' user='postgres' password='postgres'")
 except:
     print "I am unable to connect to the database."
 
-if 'test_recommendations.csv' is '':
-    print "\n\n\tYou need to pull down the recommendation CSV from google drive"
+if 'loading.csv' is '':
+    print "\n\n\tYou need to have a csv file named 'loading.csv' in this directory"
 
 cur = conn.cursor()
-sql = "SELECT id FROM employers"
+sql = "SELECT id FROM loading_table"
 cur.execute(sql)
 rows = cur.fetchall()
 employers = []
@@ -21,16 +21,16 @@ for row in rows:
     employers.append(row[0])
 
 
-with open('test_recommendations.csv' , 'rb') as csvfile:
+with open('loading.csv' , 'rb') as csvfile:
     reader = csv.DictReader(csvfile)
     header_names = reader.fieldnames
     for row in reader:
         triggers.append(row['Row Titles'])
 
 final_object = []
-fieldnames = ['Recommendation', 'Source', 'Category','Score']
+fieldnames = ['column1', 'column2', 'column3', 'column4']
 final_object.append(fieldnames)
-with open('test_recommendations.csv' , 'rb') as csvfile:
+with open('loading.csv' , 'rb') as csvfile:
     reader = csv.DictReader(csvfile)
     for header in header_names[1:]:
         val = 0
@@ -40,10 +40,10 @@ with open('test_recommendations.csv' , 'rb') as csvfile:
             val += 1
         csvfile.seek(0)
 
-with open('recommendation_import.csv', 'wb') as writefile:
+with open('loading.csv', 'wb') as writefile:
     output = csv.writer(writefile, delimiter=',')
     for row in final_object:
         output.writerow(row)
 
-print "\n\n\tYou have successfully created the custom recommendations."
-print "\n\n\tcopy and paste this in your terminal -> python import_recommendations.py\n\n"
+print "\n\n\tYou have successfully loaded the files."
+print "\n\n\tcopy and paste this in your terminal -> python import_loading.py\n\n"
